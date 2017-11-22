695b11bddc84e1538caf146fb4753c4ffda4f496
iago64 <facchinidm@gmail.com>
2017-11-22 11:07:16 -0300

    Ajuste en iniciales

--- transformador_Iniciales.py
+++ transformador_Iniciales.py
@@ -6,7 +6,7 @@ def getRegistro(linea):
          if(len(linea.split(',')[0]) > 0 and len(linea.split(',')[1]) > 0):
              try:
                  inicial = linea.replace(' ', '').split(',')[0][:1]
-                 registro = linea.replace(' ', '').split(',')[0] + "," + linea.replace(' ', '').split(',')[1]+ "," + linea.replace(' ', '').split(',')[2] + "\n"
+                 registro = inicial + "," + linea.replace(' ', '').split(',')[1]+ "," + linea.replace(' ', '').split(',')[2] + "\n"
                  sys.stdout.write(registro)
              except Exception:
                  pass
